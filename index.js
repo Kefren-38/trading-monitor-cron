a0a09a1aff967b1cc5746d70f4d46ff7db366eaa
Kefren-38 <72788479+Kefren-38@users.noreply.github.com>
2025-08-09 16:40:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const serviceAccount = {
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
   client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40buildtradeacademy.iam.gserviceaccount.com"
 };
- 
+  
 // Initialiser Firebase Admin
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
